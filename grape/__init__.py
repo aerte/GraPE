2263142a4112d373921bf1cc8025e45e883d0450
paulseghers <0099seghers@gmail.com>
2024-05-13 01:25:23 +0200

    fixed circular imports (unnecessary, but leaving as comms if it was done for a reason)

--- grape/__init__.py
+++ grape/__init__.py
@@ -1,6 +1,6 @@
-from grape import models as models
-from grape import splits as splits
-from grape import plots as plots
-from grape import utils as utils
-from grape import datasets as datasets
-from grape import analysis as analysis
+# from grape import models as models
+# from grape import splits as splits
+# from grape import plots as plots
+# from grape import utils as utils
+# from grape import datasets as datasets
+# from grape import analysis as analysis
