9fdd55c41e39f5138d2b4a2662f98db7fae0244d
aerte <62424399+aerte@users.noreply.github.com>
2024-05-30 09:56:22 +0200

    fixed stuff

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='grape',
-    version='0.0.2',
+    version='0.0.3',
     author='Felix Ærtebjerg',
     description='tools for computational chemistry and deep learning',
     packages=['grape'],  # List of packages to be included
