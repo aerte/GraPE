28e4c4cd957d15d8e32ecb039032f4badd394b63
aerte <62424399+aerte@users.noreply.github.com>
2024-05-30 09:55:31 +0200

    fixed stuff

--- python/grape/__init__.py
+++ python/grape/__init__.py
@@ -1,7 +1,7 @@
-# from grape import models as models
+from grape import models as models
 # from grape import splits as splits
 # from grape import plots as plots
-from grape import utils as utils
+# from grape import utils as utils
 # from grape import datasets as datasets
 # from grape import analysis as analysis
 # from grape import optim as optim
