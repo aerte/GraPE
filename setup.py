381a7da56799d2913b5555597b68d6d7036ec7a3
paulseghers <0099seghers@gmail.com>
2024-06-26 00:54:20 +0200

    remove annoying old deletions

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-setup(
-    name='grape',
-    version='1.0.0',
-    author='Felix Ærtebjerg',
-    description='tools for computational chemistry and deep learning',
-    packages=['grape'],  # List of packages to be included
-)
