60e0b872e9ee21fc8b66740e573f447b2bf26e5f
Thomas Swagerman <thomasswagerman@hotmail.com>
2024-09-28 15:54:57 +0200

    add function for logging latents as artifacts

--- src/grape_chem/logging/mlflow_logging.py
+++ src/grape_chem/logging/mlflow_logging.py
@@ -1,5 +1,6 @@
 import mlflow
 from typing import Dict
+import pandas as pd
 
 
 def setup_run_name(config: Dict):
@@ -24,3 +25,15 @@ def setup_mlflow(config:Dict):
 
 def log_params_to_mlflow(params):
     mlflow.log_params(params)
+
+def log_latents(latents, file_path, log_latent_batch_index=0):
+    # Convert latents to NumPy for logging
+    latents_to_log = latents.cpu().detach().numpy()  # Ensure it's on CPU and converted to NumPy
+    latent_file_path = file_path + 'latents.csv'
+    
+    # Save the latents to a CSV file
+    pd.DataFrame(latents_to_log).to_csv(latent_file_path, index=False)
+
+    if mlflow.active_run():
+        # Log the latent representation as an artifact
+        mlflow.log_artifact(latent_file_path)
