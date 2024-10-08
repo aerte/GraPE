cb40ac166a91d0eca7a059934362a44e30143d33
paulseghers <0099seghers@gmail.com>
2024-10-08 21:05:22 +0200

    saved hyperparams for  groupgat on solvation dataset

--- notebooks/data_files/network_configs.py
+++ notebooks/data_files/network_configs.py
@@ -67,3 +67,58 @@ GroupGAT_pka = {
               "L3_hidden_dim": 64,
               }
 }
+
+GroupGAT_Vc = {
+"""
+this is coincidentally the same config that acheived good-ish performance
+on the Solvation dataset with only the first layer (L1/origin) of the
+GroupGAT model.
+"""
+    'training params': {
+        "epochs":1000,
+        "batch_size":700,
+        "patience":30,
+        "hidden_dim":47,
+        "learning_rate":0.001054627,
+        "weight_decay":1e-4,
+        "mlp_layers":2,
+        "atom_layers":3,
+        "mol_layers":3,
+    },
+    'net_params': {
+                  "device": device, 
+                  "num_atom_type": 44, # == node_in_dim
+                  "num_bond_type": 12, # == edge_in_dim
+                  "dropout": 0.0,
+                  "MLP_layers":mlp_layers,
+                  "frag_dim": None, #get from fragmentation instance
+                  "final_dropout": 0.119,
+                  "global_features": True,
+                # for origins:
+                  "num_heads": 1,
+                # for AFP:
+                  "node_in_dim": 44, 
+                  "edge_in_dim": 12, 
+                  "num_global_feats":1, 
+                  "hidden_dim": hidden_dim, #Important: check matches with `L1_hidden_dim`
+                  "mlp_out_hidden": mlp, 
+                  "num_layers_atom": atom_layers, 
+                  "num_layers_mol": mol_layers,
+                # for channels:
+                  "L1_layers_atom": 4, #L1_layers
+                  "L1_layers_mol": 1,  #L1_depth
+                  "L1_dropout": 0.142,
+
+                  "L2_layers_atom": 2, #L2_layers
+                  "L2_layers_mol": 3,  #2_depth
+                  "L2_dropout": 0.255,
+
+                  "L3_layers_atom": 1, #L3_layers
+                  "L3_layers_mol": 4,  #L3_depth
+                  "L3_dropout": 0.026,
+
+                  "L1_hidden_dim": 247,
+                  "L2_hidden_dim": 141,
+                  "L3_hidden_dim": 47,
+    }
+}
